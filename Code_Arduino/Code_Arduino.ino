adba47f8a99a6d29c493550133e0a5574b41fe26
snh31d3r <snh31d3r@gmail.com>
2017-12-01 23:04:45 -0500

    Agregando nueva funcionalidad

--- Code_Arduino/Code_Arduino.ino
+++ Code_Arduino/Code_Arduino.ino
@@ -1,7 +1,8 @@
-#include <Keypad.h>
-#include <LiquidCrystal.h>
-#include <stdlib.h>
-#include <math.h>
+#include <Keypad.h> ///Libreria del Teclado
+#include <LiquidCrystal.h> ///Libreria del Display
+#include <EEPROM.h> ///Libreria de la memoria EEPROM
+#include <stdlib.h> ///Liberia Estandar
+#include <math.h> ////Libreria para calculos matematicos
 
 
 LiquidCrystal lcd (8, 9, 10, 11, 12, 13); ///Se asigna los puertos que va a utilizar el display.
@@ -27,10 +28,11 @@ byte colPins[COLS] = {22, 24, 26, 28};
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
-int i, auxAcceso;
-char datom, menu;
-String nom, apel, cargo;
-int clave[4];
+int i, retardo = 30, idUsuario = 0; /// i: auxiliar. retardo: pausas. idUsuario: 1,2 o 3
+char datom, menu;////datom: dato de memoria.
+String nom, apel, cargo;/////nom: nombre. apel: apellido. cargo: diseño, analisis....
+int clave[4], dato[2];////clave: almacena las claves ingresadas. dato: almacena los datos
+                      ///leidos en la EEPROM.
 
 void teclado(int auxTeclado)
 {
@@ -116,6 +118,335 @@ void teclado(int auxTeclado)
   }
 }
 
+void MenuInicio() ///Menu de inicio del Diseñador o Administrador
+{
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("BIENVENIDO AL ");
+  lcd.setCursor(0, 1);
+  lcd.print("MENU DISENO: ");
+  delay(retardo);
+
+  do
+  {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("DIGITE 1:PERFIL");
+    lcd.setCursor(0, 1);
+    lcd.print(" DE JOYA ");
+    delay(retardo);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("DIGITE 2:PERFIL");
+    lcd.setCursor(0, 1);
+    lcd.print(" DE DIEGO ");
+    delay(retardo);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("DIGITE 3:PERFIL");
+    lcd.setCursor(0, 1);
+    lcd.print(" DE ANGELO ");
+    delay(retardo);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(" DIGITE 4: SALIR");
+    lcd.setCursor(0, 1);
+    lcd.print(" DEL MENU ");
+    delay(retardo);
+
+    teclado(2);// 2: Teclado para escoger la opcion; salir del menu etc...
+    delay(retardo);
+    menu = clave[0];
+
+    switch (menu)
+    {
+
+      case 1:
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("MENU DE");
+        lcd.setCursor(0, 1);
+        lcd.print("WILMER JOYA");
+        delay(retardo);
+        idUsuario = 0;
+        MenuPerfil();
+        break;
+
+      case 2:
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("MENU DE");
+        lcd.setCursor(0, 1);
+        lcd.print("DIEGO AREVALO");
+        delay(retardo);
+        idUsuario = 1;
+        MenuPerfil();
+        break;
+
+      case 3:
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("MENU DE");
+        lcd.setCursor(0, 1);
+        lcd.print("ANGELLO TRIVINO");
+        delay (retardo);
+        idUsuario = 2;
+        MenuPerfil();
+        break;
+
+      case 4:
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("SALIENDO DEL");
+        lcd.setCursor(0, 1);
+        lcd.print("MENU PERFIL");
+        delay (retardo);
+        break;
+
+      default:
+        lcd.clear();
+        lcd.print("ERROR MARQUE UN");
+        lcd.setCursor(0, 1);
+        lcd.print("NUMERO, 1 AL 4");
+        delay (retardo);
+    }
+
+  } while (menu != 4);
+
+  loop();
+}
+
+void MenuPerfil () { /////Menú perfil de cada usuario.
+  do {
+
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("DIGITE 1:NUEVA");  ///Cambio de contraseña del usuario
+    lcd.setCursor(0, 1);
+    lcd.print(" CONTRASENA ");
+    delay (retardo);
+
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("DIGITE 2:");
+    lcd.setCursor(0, 1);
+    lcd.print("SALIR AL MENU");
+    delay(retardo);
+
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("DIGITE 3:");
+    lcd.setCursor(0, 1);
+    lcd.print("SALIR AL INICIO ");
+    delay(retardo);
+    teclado(2);
+    delay(retardo);
+    menu = clave[0];
+    switch (menu)
+    {
+
+      case 1: ////Nueva contraseña
+        i = 0;
+        do {
+          lcd.clear();
+          lcd.setCursor(0, 0);
+          lcd.print("DIGITE NEW");
+          lcd.setCursor(0, 1);
+          lcd.print("CONTRASENA:");
+          delay(retardo);
+          teclado(1);
+          dato[0] = clave[0];
+          dato[1] = clave[1];
+          lcd.clear();
+          lcd.setCursor(0, 0);
+          lcd.print("CONFIRME");
+          lcd.setCursor(0, 1);
+          lcd.print("CONTRASENA:");
+          delay(retardo);
+          teclado(1);
+          if (CompararDatos() == 2) {
+            EscribirEEPROM();
+            delay(retardo);
+            lcd.clear();
+            lcd.setCursor(0, 0);
+            lcd.print("CONTRASENA");
+            lcd.setCursor(0, 1);
+            lcd.print("GUARDADA");
+            delay(retardo);
+            i = 1;
+          } else
+          {
+            lcd.clear();
+            lcd.setCursor(0, 0);
+            lcd.print("CONTRASENAS");
+            lcd.setCursor(0, 1);
+            lcd.print("NO COINCIDEN");
+            delay(retardo);
+          }
+
+        } while (i != 1);
+
+        break;
+
+      case 2:
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("VOLVIENDO");
+        lcd.setCursor(0, 1);
+        lcd.print("MENU PERFIL");
+        delay (retardo);
+        MenuInicio();
+        break;
+
+      case 3:
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("VOLVIENDO");
+        lcd.setCursor(0, 1);
+        lcd.print("AL INICIO");
+        delay (retardo);
+        loop();
+        break;
+
+      default:
+        lcd.clear();
+        lcd.print("ERROR MARQUE UN");
+        lcd.setCursor(0, 1);
+        lcd.print("NUMERO, 1 AL 3");
+        delay (retardo);
+
+    }
+
+  } while (menu != 2 || menu != 3);
+
+}
+
+void MenuUsuario() { /////Menu del usuario.
+
+  if (idUsuario == 0) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("BIENVENIDO");
+    lcd.setCursor(0, 1);
+    lcd.print("WILMER JOYA");
+    delay(retardo);
+  } else if (idUsuario == 1) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("BIENVENIDO");
+    lcd.setCursor(0, 1);
+    lcd.print("DIEGO AREVALO");
+    delay(retardo);
+  } else if (idUsuario == 2) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("BIENVENIDO");
+    lcd.setCursor(0, 1);
+    lcd.print("ANGELO TRIVINO");
+    delay(retardo);
+  }
+
+  do {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("DIGITE 1:");
+    lcd.setCursor(0, 1);
+    lcd.print("SALIR ");
+    delay(retardo);
+    teclado(2);
+    delay(retardo);
+    menu = clave[0];
+    switch (menu)
+    {
+
+      case 1:
+        i = 0;
+
+        break;
+
+      default:
+        lcd.clear();
+        lcd.print("ERROR MARQUE UN");
+        lcd.setCursor(0, 1);
+        lcd.print("NUMERO, 1 SALIR");
+        delay (retardo);
+        break;
+    }
+
+  } while (menu != 1);
+
+  loop();
+}
+
+void EscribirEEPROM() ///Escribir EEPROM interna del Arduino
+{
+  int i = 0;
+  if (idUsuario == 0)
+    i = 0;
+  else if (idUsuario == 1)
+    i = 2;
+  else if (idUsuario == 2)
+    i = 4;
+
+  int k = 0;
+  for (int j = i; j < i + 2; j++)
+  {
+    EEPROM.write(j, clave[k]);
+    k++;
+  }
+
+}
+
+void LeerEEPROM() ///Leer EEPROM Interna del Arduino
+{
+  int i = 0;
+  if (idUsuario == 0)
+    i = 0;
+  else if (idUsuario == 1)
+    i = 2;
+  else if (idUsuario == 2)
+    i = 4;
+
+
+  int k = 0;
+  for (int j = i; j < i + 2; j++)
+  {
+    dato[k] = EEPROM.read(j);
+    k++;
+  }
+
+}
+
+bool ComprobarUsuario() {//Comprueba el usuario
+  bool comp = false;
+
+  for (int i = 0; i < 3; i++) { ////Lee la EEPROM cuatro veces por ser 4 digitos de la clave
+    idUsuario = i;
+    LeerEEPROM();
+
+    if ((clave[0] == dato[0]) && (clave[1] == dato[1])) {
+
+      comp = true;
+      break;
+    }
+  }
+
+  return comp;
+
+}
+
+int CompararDatos() ////Compara los datos ingresados con los almacenados en la EEPROM 
+{
+  int auxComp = 0;
+  for (int i = 0; i < 2; i++) {
+    if (dato[i] == clave[i])
+      auxComp++;
+  }
+
+  return auxComp;
+}
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -125,94 +456,58 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("  BIENVENIDOS A");
 
   lcd.setCursor(0, 1);
-  lcd.print("PROGRAMACION ARDUINO");
-  delay(1000);
-
+  lcd.print("PROGRAMA ARDUINO");
+  delay(retardo);
+  lcd.clear();//limpialcd
+  lcd.setCursor(0, 0);
+  lcd.print(" ANGELO  ");
+  lcd.setCursor(0, 1);
+  lcd.print(" TRIVINO ");
+  delay(retardo);
+  lcd.clear();//limpialcd
+  lcd.setCursor(0, 0);
+  lcd.print(" DIEGO ");
+  lcd.setCursor(0, 1);
+  lcd.print(" AREVALO ");
+  lcd.setCursor(0, 1);
+  delay(retardo);
+  lcd.clear();//limpialcd
+  lcd.setCursor(0, 0);
+  lcd.print(" WILMER ");
+  lcd.setCursor(0, 1);
+  lcd.print(" JOYA ");
+  lcd.setCursor(0, 1);
+  delay(retardo);
+  lcd.clear();//limpialcd
   teclado(1); // 1: Teclado para el password
-  delay(100);
-  auxAcceso = 0;
+  delay(retardo);
 
-  if ((clave[0] == 0) && (clave[1] == 0)) ///Si el valor ingresado es igual al password almacenado entra en Analisis
+  if ((clave[0] == 0) && (clave[1] == 0))
   {
-    nom = "ANGELLO"; apel = "TRIVINO"; cargo = "ANALISIS";
-    auxAcceso = 1;
-  } else if ((clave[0] == 12) && (clave[1] == 34)) ///Entra en Diseño
-  {
-    nom = "DIEGO"; apel = "AREVALO"; cargo = "DISENIO";
-    auxAcceso = 1;
-  } else if ((clave[0] == 12) && (clave[1] == 35)) ///entra en Desarrollo
-  {
-    nom = "WILMER"; apel = "JOYA"; cargo = "DESARROLLO";
-    auxAcceso = 1;
-  }
+    ///Menu del Diseñador o Administrador, se encarga de cambiar los Passwords
+    MenuInicio();
 
-  if (auxAcceso == 1) ///si es correcto el password entra.
-  {
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("BIENVENIDO AL ");
-    lcd.setCursor(0, 1);
-    lcd.print("MENU: " + cargo);
-    delay(1500);
+  } else {
+    
+    LeerEEPROM(); ///Si el anterior es incorrecto, busca las contraseñas de los usuarios.
+    
+    if (ComprobarUsuario()) {
+      MenuUsuario();
 
-    do
-    {
+    } else { //// Si no encuentra ninguna en la EEPROM,la contraseña es incorrecta.
       lcd.clear();
       lcd.setCursor(0, 0);
-      lcd.print(" DIGITE 1: MENU");
+      lcd.print(" PASSWORD");
       lcd.setCursor(0, 1);
-      lcd.print(" DE " + nom + " O");
-      delay(1000);
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print(" 2: PARA SALIR");
-      delay(1000);
-
-
-      teclado(2);// 2: Teclado para escoger la opcion; salir del menu etc...
-      delay(100);
-      menu = clave[0];
-
-      switch (menu)
-      {
-        case 1:
-          lcd.clear();
-          lcd.setCursor(0, 0);
-          lcd.print("BIENVENIDO");
-          lcd.setCursor(0, 1);
-          lcd.print(nom + " " + apel);
-          delay(1000);
-          break;
-
-        case 2:
-          lcd.clear();
-          lcd.print("  SALIENDO");
-          lcd.setCursor(0, 1);
-          lcd.print("  DEL MENU");
-          delay(1000);
-          break;
-
-        default:
-          lcd.clear();
-          lcd.print("ERROR MARQUE UN");
-          lcd.setCursor(0, 1);
-          lcd.print("NUMERO, 1 O 2");
-          delay(1000);
-      }
-
-    } while (menu != 2);
-
-  } else {  ////si no es correcto el password, da mensaje de error.
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print(" PASSWORD");
-    lcd.setCursor(0, 1);
-    lcd.print(" INCORRECTO");
-    delay(1000);
+      lcd.print(" INCORRECTO");
+      delay (retardo);
+    }
   }
+  
 }
--- README.md
+++ README.md
@@ -3,13 +3,13 @@ Simula el acceso a un menú de un Laboratorio de Electrónica, usando Proteus(Si
 
 
 ## ¿Cómo funciona?
-### Versión 1.0
+### Versión 1.1
 
-El código está desarrollado en Arduino(IDE) y probado y simulado en Proteus. Los elementos visuales son: Arduino Mega 2560 R, Display LM016L (Pines: 8,9,10,11,12,13), Resistencia 1k ohmios y teclado KeyPad-Phone 4x4 (pines(Rows): 30,32,34,36 y Pines(Columns):22,24,26,28). (Imagen-1)
+El código está desarrollado en Arduino(IDE), probado y simulado en Proteus. Los elementos visuales son: Arduino Mega 2560 R, Display LM016L (Pines: 8,9,10,11,12,13), Resistencia 1k ohmios y teclado KeyPad-Phone 4x4 (pines(Rows): 30,32,34,36 y Pines(Columns):22,24,26,28). (Imagen-1)
 
 ![img-1](Images_Readme/Prueba_1.png).
 
 ## Ejemplo del funcionamiento
-Al iniciar la simulación muestra una bienvenida, luego pide un password y inicia el menú del Analista. (Imagen-2)
+Al iniciar la simulación muestra una bienvenida, luego pide un password y inicia el menú de diseño o Administrador. La versión 1.1 permite modificar el password del usuario y lo guarda en la memoria interna EEPROM del Arduino (Imagen-2)
 
 ![ima-2](Images_Readme/Func_1.gif)
